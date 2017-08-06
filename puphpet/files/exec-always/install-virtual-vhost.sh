55b1d5a11da458da71569ebef72e6503e3833d02
Antoine <antoine@2ec0b4.fr>
2017-08-06 18:57:26 +0200

    Mise à jour de la box avec Ubuntu 16.04

--- puphpet/files/exec-always/install-virtual-vhost.sh
+++ puphpet/files/exec-always/install-virtual-vhost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VAGRANT_CORE_FOLDER=$(cat '/.puphpet-stuff/vagrant-core-folder.txt')
-DEFAULT_VHOST=$VAGRANT_CORE_FOLDER"/files/exec-always/000-default.conf"
+PUPHPET_CORE_DIR=/opt/puphpet
+DEFAULT_VHOST=$PUPHPET_CORE_DIR"/files/exec-always/000-default.conf"
 
 cp "$DEFAULT_VHOST" /etc/apache2/sites-available/000-default.conf
 a2ensite 000-default.conf
